be6f23fccde692ee908320cde1b80ab07297d733
bruno <bruno.peron@unochapeco.edu.br>
2021-05-10 21:47:54 -0300

    angular

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogsComponent } from './logs/logs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
